212d302e21576bbcc1b9e48e5a0ad87207b67276
BillboEliasJohansson <153284819+BillboEliasJohansson@users.noreply.github.com>
2024-11-11 10:48:44 +0100

    Update constants.dart
    Removed API Keys

--- lib/Constants/constants.dart
+++ lib/Constants/constants.dart
@@ -1,6 +1,6 @@
-const String google_api_key = "AIzaSyB5mSpGiD6KzsDSlJcPss9H3cv0kgiCN1k";
+const String google_api_key = "";
 
-const String weather_api_key = "bfb8ca3b1596c34f415795b867370d03";
+const String weather_api_key = "";
 //ERROR MESSAGES
 const String emptyPassword = "Lösenord kan inte vara tomt";
 
